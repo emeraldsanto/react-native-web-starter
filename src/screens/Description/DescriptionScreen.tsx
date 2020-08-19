fc325a83bcc5d070b5b73d6fd105b18367478431
emeraldsanto <yanick.belanger@yahoo.com>
2020-08-19 19:20:48 -0400

    Added styles and content to second screen

--- src/screens/Description/DescriptionScreen.tsx
+++ src/screens/Description/DescriptionScreen.tsx
@@ -1,13 +1,35 @@
 import React from "react";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 import { Route } from "../../models/enums/Route";
 import { NavigationScreen } from "../../navigation/NavigationScreen";
 import { DescriptionScreenStyle } from "./DescriptionScreenStyle";
 
+const TECHNOLOGIES = [
+	"ReactJS",
+	"React Native",
+	"React Native Web",
+	"React Navigation",
+	"TypeScript"
+]
+
 export const DescriptionScreen: NavigationScreen<Route.DESCRIPTION> = () => {
 	return (
 		<View style={DescriptionScreenStyle.container}>
+			<Text>
+				This application is built using the following technologies:
+			</Text>
 
+			<View style={DescriptionScreenStyle.list}>
+				{TECHNOLOGIES.map(t => (
+					<View style={DescriptionScreenStyle.point}>
+						<View style={DescriptionScreenStyle.bullet} />
+
+						<Text>
+							{t}
+						</Text>
+					</View>
+				))}
+			</View>
 		</View>
 	);
 }
--- src/screens/Description/DescriptionScreenStyle.ts
+++ src/screens/Description/DescriptionScreenStyle.ts
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import { Platform, StyleSheet } from "react-native";
 
 export const DescriptionScreenStyle = StyleSheet.create({
 	container: {
@@ -6,5 +6,21 @@ export const DescriptionScreenStyle = StyleSheet.create({
 		padding: 25,
 		alignItems: "center",
 		justifyContent: "center"
+	},
+	list: {
+		width: Platform.OS === "web" ? undefined : "100%",
+		marginVertical: 15,
+		paddingHorizontal: 15
+	},
+	point: {
+		alignItems: "center",
+		flexDirection: "row"
+	},
+	bullet: {
+		width: 5,
+		height: 5,
+		marginEnd: 5,
+		borderRadius: 5,
+		backgroundColor: "#000000"
 	}
 });
